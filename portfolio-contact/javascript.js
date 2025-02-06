fc3dccea5dfe6b17d630294e46a74bdd9a2e1631
Tanish-Nagari <nagaritanish1234@gmail.com>
2025-02-07 00:33:49 +0530

    Add files via upload

---
+++ portfolio-contact/javascript.js
@@ -0,0 +1,4 @@
+document.querySelector('.contact-form').addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevent form from reloading the page
+    alert('Thank you for reaching out! Your message has been sent.');
+});
